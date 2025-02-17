77e05e1bf58bb53f125c4dd714f0db1acf695aa3
leydet4 <109769769+leydet4@users.noreply.github.com>
2025-02-17 15:36:33 -0500

    Update and rename src/App.jsx to src/components/App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import TripDashboard from "./components/TripDashboard";
+import TripDashboard from "./TripDashboard";
 
 export default function App() {
   return (
     <div>
       <h1>CFD-FB4-BRNKL Dashboard</h1>
-      <TripDashboard videoSrc="/path-to-your-video.mp4" tripData={[]} />
+      <TripDashboard />
     </div>
   );
 }
