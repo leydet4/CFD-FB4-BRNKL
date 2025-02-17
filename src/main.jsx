beba7c1ee779498dd2c863280998817dd929bdce
leydet4 <109769769+leydet4@users.noreply.github.com>
2025-02-17 15:30:24 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App"; // ✅ Ensure this path is correct
+import App from "./src/App"; // ✅ Ensure this path is correct
 import "leaflet/dist/leaflet.css"; // If using Leaflet
 
 ReactDOM.createRoot(document.getElementById("root")).render(
