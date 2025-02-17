ce5b2940d57ef1a0c60dad68b4b35ac4df2dc3da
leydet4 <109769769+leydet4@users.noreply.github.com>
2025-02-17 11:53:43 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "./",  // Ensure Vite finds the correct root directory
+  build: {
+    outDir: "dist",
+  },
 });
